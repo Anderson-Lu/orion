cbb23d5c127dec353f4d6af924c337f119c24ffc
andersonlu <andersonlu@tencent.com>
2024-09-20 16:35:14 +0800

    v0.0.2-updated

--- pkg/xgrpc/build/build.go
+++ pkg/xgrpc/build/build.go
@@ -9,13 +9,15 @@ var (
 	BuildTime      = ""
 	BuildCommit    = ""
 	BuildGoVersion = ""
+	XGRPCVersion   = "dev0.0.2"
 )
 
 func init() {
 	fmt.Print("-----\n")
-	fmt.Printf("XGRPC Branch     : %s \n", BuildVersion)
-	fmt.Printf("XGRPC Commit     : %s \n", BuildCommit)
-	fmt.Printf("XGRPC BuiltTime  : %s \n", BuildTime)
-	fmt.Printf("XGRPC GO Version : %s \n", BuildGoVersion)
+	fmt.Printf("Git Branch     : %s \n", BuildVersion)
+	fmt.Printf("Git Commit     : %s \n", BuildCommit)
+	fmt.Printf("Built Time     : %s \n", BuildTime)
+	fmt.Printf("Go Version     : %s \n", BuildGoVersion)
+	fmt.Printf("XGRPC Version  : %s \n", XGRPCVersion)
 	fmt.Print("-----\n")
 }
